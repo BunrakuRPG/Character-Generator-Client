d43a3de434e7fcaad69aba45c20988ec81ba031b
Eric Sipple <saalon@gmail.com>
2015-06-02 19:30:08 -0400

    Adding application adapter.

--- Brocfile.js
+++ Brocfile.js
@@ -7,9 +7,6 @@ var app = new EmberApp();
 // Use `app.import` to add additional libraries to the generated
 // output files.
 
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
-app.import('vendor/bootstrap/dist/css/bootstrap.css');
-
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
 // object's keys should be the environment name and the values
