ff75e91c300376ce92ea75eb72f6198d2b046568
Eric Sipple <saalon@gmail.com>
2015-06-02 22:09:18 -0400

    Sass installed, bootstrap importing, and application template wrapped in a fluid container.

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,13 @@
 /* global require, module */
 
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
-
+//
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
 // object's keys should be the environment name and the values
